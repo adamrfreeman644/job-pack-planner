beda50034371acdac04f4264542eb8f9801074dc
adamrfreeman644 <adamfreeman644@gmail.com>
2026-09-15 18:08:05 +0100

    Remove Outlook scheduler

--- scheduler.py
+++
@@ -1,6 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-from app import pull_outlook
-
-scheduler=BlockingScheduler(timezone='Europe/London')
-scheduler.add_job(pull_outlook,'cron',hour=19,minute=0,id='outlook_daily',max_instances=1,coalesce=True)
-scheduler.start()
